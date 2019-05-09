482614cd60fff14be6cf216c2f038a268cf6f5f2
Yanbuaban <remi.yanbuaban@gmail.com>
2019-05-09 21:41:03 +0200

    Les événements

--- CoursC#.cs
+++ CoursC#.cs
@@ -588,8 +588,27 @@ reader.Close();
 // p2 est maintenant utilisable comme une class
 Console.WriteLine(p2.Nom + " " + p2.Prenom);
 
+--------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------- Ref / Out / Delegate / Func / Action / Expression Lambda ------------------------------------------------------------------------
 
 
+------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------- Les événements ----------------------------------------------------------------------------
+
+// On appelle évènement une action particulière qui a lieu. Cela peut être un clic de souris, l'appui d'une touche du clavier, ... 
+// Il existe une infinité d'évènements puisque vous pouvez même créer les vôtres.
+
+But
+// Ce que l'on veut, c'est effectuer une action lorsqu'un certain évènement est déclenché. Pour cela, il faut déjà savoir quand est déclenché l'évènement dont il est question.
+// C'est ici qu'entrent en jeu les gestionnaires d'évènements ("event handlers" en anglais). Il servent à indiquer que l'évènement surveillé vient d'être déclenché.
+
+Fonctionnement
+// Le principe est simple : on attache un gestionnaire d'évènements à un évènement (on dit que l'on s'abonne à l'évènement) en spécifiant une méthode. 
+// Cette méthode est appelée lorsque l'évènement est déclenché. 
+
+
+------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------- Les exceptions ----------------------------------------------------------------------------
 
 
 
