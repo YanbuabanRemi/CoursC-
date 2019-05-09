89fb77e8606fa68c80e95f9b84b6acdd03b19a4a
Yanbuaban <remi.yanbuaban@gmail.com>
2019-05-09 23:49:12 +0200

    ADONET Déconnecter rectif

--- CoursC#.cs
+++ CoursC#.cs
@@ -645,38 +645,54 @@ supprimer une table:
 // DROP TABLE xxx
 
 modifier une table:
-(en ajoutant)
-// ALTER TABLE xxx ADD variable valeur....
-(en supprimant)
-// ALTER TABLE xxx DROP COLUMN variable
+// (en ajoutant)
+ALTER TABLE xxx ADD variable valeur....
+// (en supprimant)
+ALTER TABLE xxx DROP COLUMN variable
 
 - Le langage de Manipulation
 
-insertion de données
-// INSERT INTO xxx (variable, variable1, variable2) VALUES (donnée, donnée1, donnée2)
+//insertion de données
+INSERT INTO xxx (variable, variable1, variable2) VALUES (donnée, donnée1, donnée2)
 
-mise à jour des données
-// UPDATE xxx set variable = donnée3, variable2 = donnée4 WHERE variable1 = donnée1
+//mise à jour des données
+UPDATE xxx set variable = donnée3, variable2 = donnée4 WHERE variable1 = donnée1
 
-supression de données
-// DELETE FROM xxx WHERE variable1 > donnnée10
+//supression de données
+DELETE FROM xxx WHERE variable1 > donnnée10
 
 - Le langage de Selection
 
-selectionne toute la table
-// SELECT * FROM xxx
+// selectionne toute la table
+SELECT * FROM xxx
 
-selectionne les colonnes nom et prenom de la table xxx et revoie les données avec l'intituler n et p
-// SELECT nom as n, prenom as p FROM xxx
+// selectionne les colonnes nom et prenom de la table xxx et revoie les données avec l'intituler n et p
+SELECT nom as n, prenom as p FROM xxx
 
-selectionne que les id supérieur à 2
-// SELECT nom FROM xxx WHERE id>2
+// selectionne que les id supérieur à 2
+SELECT nom FROM xxx WHERE id>2
 ------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------- AdoNet Connecter ----------------------------------------------------------------------------
-1
-------------------------------------------------------------------------------------------------------------------------------------------
+
+// Le mode connecter permet de mettre à jour les données en temps réel, c'est à dire que lorsquon réalise une modification d'une données
+// Celle ci est modifier dans la base de donnée
+-----------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------- AdoNet Deconnecter ----------------------------------------------------------------------------
 
+// Le mode déconnecter nvoie une image de la base de donnée à un moment donné et peut être modifier par la suite
+// On envoie les données modifiées quand on le souhaite
+// Ces données sont donc pas mise à jour en temps réel dans la base de données mais se fait lors de l'UPDATE
 
+// 2 types d'objet en monde déconnecter :
+
+// 1/ Le SqlDataAdapter permet d'importer et d'exporter les données 
+// Fill permet d'importer les données de la base de données et les stocks dans un DataSet
+// UPDATE() permet d'exporter les données du DataSet dans la base de données ne fonction des modifications:
+// - Si on ajoute du contenu dans le DataSet, il faut initier l'INSERTCOMMAND
+// - Si on modifie du contenu dans le DataSet, il faut initier l'UPDATECOMMAND
+// - Si on supprime du contenu dans le DataSet, il faut initier le DELETECOMMAND
+
+// 2/ Le DataSet permet de stocker en local, les données de la base de donnée
+// Ces données modifiés ne sont pas mis à jour dans la  base de donnée tant qu'on a pas effectué la commande
 
 
