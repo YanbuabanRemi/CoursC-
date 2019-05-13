f6708ca22997ae1077afc981376f5b82b28bee23
Yanbuaban <remi.yanbuaban@gmail.com>
2019-05-13 23:36:20 +0200

    Ajout des delegates/ Action / Func / Expression lambda

--- CoursC#.cs
+++ CoursC#.cs
@@ -589,9 +589,181 @@ reader.Close();
 Console.WriteLine(p2.Nom + " " + p2.Prenom);
 
 --------------------------------------------------------------------------------------------------------------------------------------------
-------------------------------------------- Ref / Out / Delegate / Func / Action / Expression Lambda ------------------------------------------------------------------------
+------------------------------------------- Ref / Out / /------------------------------------------------------------------------
 
+------------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------- Les delegates ----------------------------------------------------------------------------
 
+// Ils permettent de créer des variables spéciales. Ce sont des variables qui "pointent" vers une méthode.
+// Cela enregistre une méthode dans une variable
+// Le délégué va nous permettre de définir une signature de méthode et avec lui, nous pourrons pointer vers n’importe quelle méthode qui respecte cette signature.
+// En général, on utilise un délégué quand on veut passer une méthode en paramètres d’une autre méthode.
+// Ils sont la base des évenements
+
+public class TrieurDeTableau
+{
+	// Le delegate doit prendre le même type de paramêtre que les méthodes associés
+	// Si le paramêtre de la méthode est un table, le paramètre du delegate doit être un tableau
+    private delegate void DelegateTri(int[] tableau);
+
+    private void TriAscendant(int[] tableau)
+    {
+        Array.Sort(tableau);
+    }
+
+    private void TriDescendant(int[] tableau)
+    {
+        Array.Sort(tableau);
+        Array.Reverse(tableau);
+    }
+
+    //Puis on utlilise les méthodes TriAscendant et TriDescendant au sein de la méthode DemoTri grâce au delegate
+    public void DemoTri(int[] tableau)
+    {
+        DelegateTri tri = TriAscendant;
+        tri(tableau);
+        foreach (int i in tableau)
+        {
+            Console.WriteLine(i);
+        }
+
+        tri = TriDescendant;
+        tri(tableau);
+        foreach (int i in tableau)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+// On peut encore factoriser le code en rajoutant la méthode suivante
+public class TrieurDeTableau
+{
+    private delegate void DelegateTri(int[] tableau);
+
+    private void TriAscendant(int[] tableau)
+    {
+        Array.Sort(tableau);
+    }
+
+    private void TriDescendant(int[] tableau)
+    {
+        Array.Sort(tableau);
+        Array.Reverse(tableau);
+    }
+
+    private void TrierEtAfficher(int[] tableau, DelegateTri methodeDeTri)
+	{
+    	methodeDeTri(tableau);
+    	foreach (int i in tableau)
+    	{
+    	    Console.WriteLine(i);
+    	}
+	}
+
+    public void DemoTri(int[] tableau)
+    {
+		TrierEtAfficher(tableau, delegate(int[] leTableau)
+        {
+            Array.Sort(leTableau);
+        });
+
+        Console.WriteLine();
+
+        TrierEtAfficher(tableau, delegate(int[] leTableau)
+        {
+            Array.Sort(leTableau);
+            Array.Reverse(leTableau);
+        });
+    }
+}
+
+---------------------------------------- Func / Action --------------------------------------------------
+
+// Les délégués Aciton et Func osnt des délégués générique
+
+// Action est un délégué qui permet de pointer vers une méthode qui ne renvoie rien et qui peut accepter jusqu’à 16 types différents.
+private delegate void DelegateTri(int[] tableau);
+private void TrierEtAfficher(int[] tableau, DelegateTri methodeDeTri)
+	{
+    	methodeDeTri(tableau);
+    	foreach (int i in tableau)
+    	{
+    	    Console.WriteLine(i);
+    	}
+	}
+
+// Peut etre remplacer par :
+
+private void TrierEtAfficher(int[] tableau, Action<int[]> methodeDeTri)
+    {
+        methodeDeTri(tableau);
+        foreach (int i in tableau)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+// La différence se situe au niveau du paramètre de la méthode TrierEtAfficher qui prend un Action<int[]>. 
+// En fait, cela est équivalent à créer un délégué qui ne renvoie rien et qui prend un tableau d’entier en paramètre.
+// Si notre méthode avait deux paramètres, il aurait suffi d’utiliser la forme de Action avec plusieurs paramètres génériques, 
+// par exemple Action<int[], string> pour avoir une méthode qui ne renvoie rien et qui prend un tableau d’entier et une chaine de caractères en paramètres.
+
+// Lorsque la méthode renvoie quelque chose, on peut utiliser le délégué Func<T>, sachant qu’ici, c’est le dernier paramètre générique qui sera du type de retour du délégué.
+
+public class Operations
+{
+    public void DemoOperations()
+    {
+        double division = Calcul(delegate(int a, int b)
+        {
+            return (double)a / (double)b;
+        }, 4, 5);
+
+        double puissance = Calcul(delegate(int a, int b)
+        {
+            return Math.Pow((double)a, (double)b);
+        }, 4, 5);
+
+        Console.WriteLine("Division : " + division);
+        Console.WriteLine("Puissance : " + puissance);
+    }
+
+    private double Calcul(Func<int, int, double> methodeDeCalcul, int a, int b)
+    {
+        return methodeDeCalcul(a, b);
+    }
+}
+
+// Ici, dans la méthode Calcul, on utilise le délégué Func pour indiquer que la méthode prend deux entiers en paramètres et renvoie un double.
+
+----------------------------------------------- Les expressions lambdas ------------------------------------------------------
+
+// Les expressions lambda permet d'écrire les délégués de façon plus simplifier
+
+private delegate void DelegateTri(int[] tableau);
+
+DelegateTri tri = delegate(int[] leTableau)
+{
+    Array.Sort(leTableau);
+};
+// S'ecrit maintenant :
+DelegateTri tri = (leTableau) => 
+{
+    Array.Sort(leTableau);
+};
+
+// L’expression lambda « (leTableau) => » se lit : « leTableau conduit à ».
+// La variable leTableau permet de spécifier le paramètre d’entrée de l’expression lambda. 
+// Ce paramètre est écrit entre parenthèses. Ici, pas besoin d’indiquer son type vu qu’il est connu par la signature associée au délégué.
+// On utilise ensuite la flèche « => » pour définir l’expression lambda qui sera utilisée. Elle s’écrit de la même façon qu’une méthode, dans un bloc de code.
+
+double division = Calcul(delegate(int a, int b)
+        {
+            return (double)a / (double)b;
+        }, 4, 5);
+// Peut s'ecrire :
+double division = Calcul((a, b) => (double)a / (double)b, 4, 5);
 ------------------------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------- Les événements ----------------------------------------------------------------------------
 
@@ -694,5 +866,122 @@ SELECT nom FROM xxx WHERE id>2
 
 // 2/ Le DataSet permet de stocker en local, les données de la base de donnée
 // Ces données modifiés ne sont pas mis à jour dans la  base de donnée tant qu'on a pas effectué la commande
+
+-----------------------------------------------------------------------------------------------------------------------------------------
+------------------------------------------- Multithreading ----------------------------------------------------------------------------
+
+// Pour utiliser les threads, il faut ajouter l'espace nom "using System.Threading" 
+// Une applicaiton demarre toujours sur le thread principal
+// Un thread est un process, un canal ou un flux qui va permettre d'éffectué des taches
+// Un thread ne peut être appelé qu'une seule fois
+// Les données entre les thrds ne se synchronise pas
+
+// Création de thread :
+Thread t1 = new Thread(MethodeThread1);
+
+static void MethodeThread1()
+        {
+           for(int i=1; i<100; i++)
+               Console.WriteLine("Je suis la methode 1 executée dans un thread secondaire "+i);
+        }
+
+// Démarrer un thread :
+t1.Start();
+
+// Création d'un thread avec des paramètres :
+// Le delegate du "new Thread" doit être un void qui n'accepte aucun paramètre.(Afficher)
+Thread t1 = new Thread(afficher);
+
+static void afficher()
+{
+	Console.WriteLine("hello");
+}
+
+Personne p1 = new Personne { Nom = "tata", Prenom = "toto" };
+t1.Start(p1);
+t2.Start(p1);
+
+// L'OS permute le token entre les threads de manière aléatoire
+
+// Si on veut passer des paramètres dans une méthode qui va passer dans un thread, ce paramètre doit être de type objet
+Thread t1 = new Thread(Afficher);
+Thread t2 = new Thread(Afficher);
+
+// Si on veut bloquer l'accèe à un thread afin que le token "finisse" le travail avant de passer à un autre thread
+// Il faut initialiser un lock, tant que celui ci n'a pas fini, le token ne peut pas partir du thread
+
+static void Afficher(object c)
+        {
+           //utilisation du lock pour synchronisation d'acces au data
+           lock(_lock)
+           {
+               Personne p = c as Personne;
+               for (int i = 1; i < 100; i++)
+               {
+                   Console.WriteLine(p.Prenom + " " + p.Nom);
+                   if (i == 20)
+                   {
+                       p.Nom = "Nom 20";
+                   }
+               }
+           }    
+        }
+
+// Un mutex à le même rôle que le lock
+// Il doit être creer, initier puis liberer
+// Personne peut acceder aux données dans le mutex tant que celui ci n'a pas été libéré
+
+private static Mutex m1 = new Mutex();
+private static Mutex m2 = new Mutex();
+
+static void init()
+        {
+           m1.WaitOne(); // le mutex est initié
+           m2.WaitOne(); // le mutex est initié
+           v1 = 10;
+           m1.ReleaseMutex(); // le mutex est libéré
+           v2 = 20;
+           m2.ReleaseMutex(); // le mutex est libéré
+        }
+
+// Le ThreadPool permet de mettre plusieurs thread dans un pool et celui regroupe tous les threads qui l'on souhaite effectué
+ThreadPool.QueueUserWorkItem(afficher, "Coucou");
+ThreadPool.QueueUserWorkItem(afficher, "bonjour");
+ThreadPool.QueueUserWorkItem(afficher, "10");     
+
+// Un sémaphore permet de verrouiller l'ordi a un nombre de thread maximum en travail
+// Comme un portier à l'entrer d'une boite de nuit qui laisse entrer seulement un nombre défini de personne
+private static SemaphoreSlim s = new SemaphoreSlim(3);
+
+static void Afficher(object c)
+        {
+           Console.WriteLine("Start Thread N° : "+c);
+           s.Wait();
+           Console.WriteLine("Thread En travail : "+c);
+           Thread.Sleep(6000);
+           Console.WriteLine("Fin du travail : " + c);
+           s.Release();
+        }
+
+// Les exeptions géré par un thread se gère à l'intérieur du thread lui même
+
+-------------------------------------------------------------- Task -----------------------------------------------------------------
+
+// Une task permet de gerer 3 problèmes:
+// - l'impossibilité de remonter les exeptions
+// - l'impossibilité d'agir sur l'état d'un thread
+// - l'impossibilité de renvoyer un résultat
+
+
+
+
+
+
+
+
+
+
+
+
 
 
